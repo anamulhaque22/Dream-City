1bef3ac11f729d096c00240396834b7d98cdf60f
anamulhaque22 <anamulah535367@gmail.com>
2022-08-06 10:07:55 +0600

    road color

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <cstdio>
-#include <windows.h>
+//#include <windows.h>
 #include <math.h>
 #define PI 3.14259265358979323846
 #include <GL/gl.h>
 #include <GL/glut.h>
-#include<MMSystem.h>
+//#include<MMSystem.h>
 
 
 void mountainArea(){
@@ -55,56 +55,24 @@ void greenBackground()
 //ground road block
 void roadBlock()
 {
+
 	glBegin(GL_POLYGON); // road back 1
-	glColor3ub(168, 169, 173);
-	glVertex2f(-0.13f, 0.40f);
-	glVertex2f(-0.13f, 0.38f);
-	glVertex2f(-0.32f, 0.31f);
-	glVertex2f(-0.35f, 0.33f);
-
-	glEnd();
-
-	glBegin(GL_POLYGON); // road back 2
-	glColor3ub(168, 169, 173);
-	glVertex2f(-0.32f, 0.31f);
-	glVertex2f(-0.29f, 0.33f);
-	glVertex2f(0.12f, 0.20f);
-	glVertex2f(0.00f, 0.16f);
-	glEnd();
-
-	glBegin(GL_POLYGON); // road back 3
-	glColor3ub(168, 169, 173);
+	glColor3ub(88, 92, 90);
 	glVertex2f(0.12f, 0.20f);
 	glVertex2f(0.00f, 0.20f);
 	glVertex2f(-0.74f, -0.80f);
 	glVertex2f(-0.49f, -0.81f);
 	glEnd();
 
-	glBegin(GL_POLYGON); // road back 4
-	glColor3ub(168, 169, 173);
+	glBegin(GL_POLYGON); // road back 2
+	glColor3ub(88, 92, 90);
 	glVertex2f(-0.74f, -0.80f);
 	glVertex2f(-0.49f, -0.81f);
 	glVertex2f(-0.40f, -1.00f);
 	glVertex2f(-0.72f, -1.00f);
 	glEnd();
 
-	glLineWidth(2);
-	glBegin(GL_LINES); // road back 1 inside
-	glColor3ub(129, 130, 134);
-	glVertex2f(-0.13f, 0.39f);
-	glVertex2f(-0.313f, 0.32f);
-
-	glEnd();
-
-	glBegin(GL_POLYGON); // road back 2 inside
-	glColor3ub(129, 130, 134);
-	glVertex2f(0.08f, 0.19f);
-	glVertex2f(0.05f, 0.16f);
-	glVertex2f(-0.31f, 0.32f);
-	glVertex2f(-0.31f, 0.33f);
-	glEnd();
-
-	glBegin(GL_POLYGON); // road back 3 inside
+	glBegin(GL_POLYGON); // road back 1 inside
 	glColor3ub(129, 130, 134);
 	glVertex2f(0.07f, 0.19f);
 	glVertex2f(0.03f, 0.19f);
@@ -112,7 +80,7 @@ void roadBlock()
 	glVertex2f(-0.57f, -0.81f);
 	glEnd();
 
-	glBegin(GL_POLYGON); // road back 4 inside
+	glBegin(GL_POLYGON); // road back 2 inside
 	glColor3ub(129, 130, 134);
 	glVertex2f(-0.66f, -0.80f);
 	glVertex2f(-0.57f, -0.81f);
