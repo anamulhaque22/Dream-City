c4c6603651a62a62e3f122dbb60a8ec7b43c313d
unmesh99 <shoaibunm25@gmail.com>
2022-08-10 02:51:00 +0600

    Cloud 1 Added

---
+++ cg project/main.cpp
@@ -0,0 +1,135 @@
+#include <cstdio>
+#include <windows.h>
+#include <math.h>
+#define PI 3.14259265358979323846
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include<MMSystem.h>
+
+
+
+GLfloat position2 = 0.0f;
+GLfloat speed2 = -0.002f;
+void cloud(int value)
+{
+    if(position2 < -1.00)
+        position2 =1.00f;
+    position2 += speed2;
+    glutPostRedisplay();
+    glutTimerFunc(100, cloud, 0);
+}
+
+void cloud1()
+{
+    int i;
+
+    GLfloat x=.5f;
+    GLfloat y=.86f;
+    GLfloat radius =.05f;
+    int triangleAmount = 20;
+    GLfloat twicePi = 2.0f * PI;
+
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3ub(255, 217, 255);
+    glVertex2f(x, y); // center of circle
+    for(i = 0; i <= triangleAmount; i++)
+    {
+        glVertex2f(
+            x + (radius * cos(i *  twicePi / triangleAmount)),
+            y + (radius * sin(i * twicePi / triangleAmount))
+        );
+    }
+    glEnd();
+
+    GLfloat a=.55f;
+    GLfloat b=.83f;
+
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3ub(255, 217, 255);
+    glVertex2f(a, b); // center of circle
+    for(i = 0; i <= triangleAmount; i++)
+    {
+        glVertex2f(
+            a + (radius * cos(i *  twicePi / triangleAmount)),
+            b + (radius * sin(i * twicePi / triangleAmount))
+        );
+    }
+    glEnd();
+
+    GLfloat c=.45f;
+    GLfloat d=.83f;
+
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3ub(255, 217, 255);
+    glVertex2f(c, d); // center of circle
+    for(i = 0; i <= triangleAmount; i++)
+    {
+        glVertex2f(
+            c + (radius * cos(i *  twicePi / triangleAmount)),
+            d + (radius * sin(i * twicePi / triangleAmount))
+        );
+    }
+    glEnd();
+
+    GLfloat e=.52f;
+    GLfloat f=.8f;
+
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3ub(255, 217, 255);
+    glVertex2f(e, f); // center of circle
+    for(i = 0; i <= triangleAmount; i++)
+    {
+        glVertex2f(
+            e + (radius * cos(i *  twicePi / triangleAmount)),
+            f+ (radius * sin(i * twicePi / triangleAmount))
+        );
+    }
+    glEnd();
+
+    GLfloat g=.6f;
+    GLfloat h=.82f;
+
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3ub(255, 217, 255);
+    glVertex2f(g, h); // center of circle
+    for(i = 0; i <= triangleAmount; i++)
+    {
+        glVertex2f(
+            g + (radius * cos(i *  twicePi / triangleAmount)),
+            h+ (radius * sin(i * twicePi / triangleAmount))
+        );
+    }
+    glEnd();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(1280, 720);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Dream City");
+	windowModel();
+
+	glutDisplayFunc(display);
+      glutTimerFunc(100, cloud, 0);
+
+
+
+	glutMainLoop();
+	return 0;
+}
